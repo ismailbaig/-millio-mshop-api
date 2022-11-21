74ff38191581acec8b86fdc78440a6033736e41e
ismailbaig <ismailece2007@gmail.com>
2022-11-21 22:38:50 +0530

    token, itemsbasedetails singleItem all items

--- customers.js
+++ customers.js
@@ -1,9 +1,15 @@
- var express = require('express');
- var router = express.Router();
+import express from 'express'
+ var router2 = express.Router();
  
  /* GET customers listing. */
- router.get('/', function (req, res, next) {
+ router2.get('/r2', function (req, res, next) {
     res.send('respond with a resource');
  });
+
+function add (x, y) {
+   return x + y;
+ }
  
- module.exports = router;
+export const addFn  = add;
+export const routerFn  = router2;
+//module.exports.add = add;
--- index.js
+++ index.js
@@ -1,44 +1,85 @@
-import express, { response } from 'express'
-import cors from 'cors'
-import mongoose from 'mongoose'
-import jwt from 'jsonwebtoken'
-//import customersRouter from 'customers'
-
-var router = express.Router();
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
+import {addFn} from './customers.js';
+import {options} from './options.js';
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(cors());
 
-
 mongoose.connect('mongodb://localhost:27017/milliodb', {
     useNewUrlparser: true,
     useunifiedTopology: true
 }, () => {
     console.log("mongo db connected!!");
-})
+});
 
-
+// Mongoose Schema - Start
 const userSchema = new mongoose.Schema({
     name: String,
     email: String,
     password: String
 });
 
+export const itembasedetailsModel = new mongoose.Schema({
+    pkItemId: Number,
+    id: Number,
+    uniqueId: String,
+    itemName: String,
+    rating: String,
+    itemCartCount: Number,
+    reviewCount: Number,
+    itemDetail1: String, 
+    itemDetail2: String, 
+    itemDetail3: String, 
+    itemDetail4: String, 
+    itemDetail5: String, 
+    itemDetail6: String, 
+    description: String,
+    discountPrice: Number,
+    actualPrice: Number,
+    imageName: String
+});
+
 const User = new mongoose.model("User", userSchema);
+const itembasedetails = new mongoose.model("itembasedetails", itembasedetailsModel);
+// Mongoose Schema - End
 
-app.get("/test", (req, res) => {
-    res.send("test");
-})
+//Application Routes - Start
+app.post("/login", (req, res) => {
+    const { uid, pd } = req.query;
+    User.findOne({name: uid, password: pd}, (err, user) => {
+        if(err) {
+            res.status(500).json({
+                message: 'Error'
+                });
+        }
+        if(user) {
+            let userdata = {
+                username: uid,
+                password: pd
+            }
+            let token = jwt.sign(userdata, options.skey, {
+                algorithm: 'HS256',
+                expiresIn: '60m'
+            });
 
-app.get("/login/:id", (req, res) => {
-    console.log(req.body);
-    console.log(req.params.id);
-    res.send({request: req.body, data: req.params.id})
-})
+            res.status(200).json({
+                message: 'Login Successful',
+                jwtoken: 'Bearer ' + token
+                });
 
-//Routes
+        }else {
+            res.status(401).json({
+                message: 'Login Failed 2'
+                });
+        }
+    });
+});
+
 app.post("/register", (req, res) => {
     console.log(req.query);
     const { name, email, password} = req.query;
@@ -57,56 +98,75 @@ app.post("/register", (req, res) => {
                 } else {
                     res.send({message: "User registered scuccessfully!!"})
                 }
-            })
+            });
         }
-    })
+    });
 });
 
-app.post("/login", (req, res) => {
-    const { uid, pd } = req.query;
-    User.findOne({name: uid, password: pd}, (err, user) => {
-        if(err) {
-            res.status(500).json({
-                message: 'Error'
-                });
-        }
-        if(user) {
-            let userdata = {
-                username: uid,
-                password: pd
-            }
-            let token = jwt.sign(userdata, "mysecretkey123", {
-                algorithm: 'HS256',
-                expiresIn: '10m'
-            });
-
-            res.status(200).json({
-                message: 'Login Successful',
-                jwtoken: 'Bearer ' + token
-                });
-
-        }else {
-            res.status(401).json({
-                message: 'Login Failed 2'
-                });
-        }
-    })
-})
-
-app.post("/testtoken", verifyToken, (req, res, next) => {
-    jwt.verify(req.token, "mysecretkey123", (err, authData) => {
+//Gets all data from Collection for Items base information
+app.get("/gtalitms", verifyToken, (req, res) => {
+    jwt.verify(req.token, options.skey, (err, data) => {
         if(err){
             res.status(403).json({
-                message: "Invalid Token validated!!",        
+                message: "Invalid Token !!",        
               });
         } else {
+            itembasedetails.find( {},  (err, result) => {
+                if (err) {
+                    res.send(err);
+                } else {
+                    res.status(200).json(result);
+                }
+            });
+        };
+    });
+});
+
+app.get("/gtsgleitm/:uid", verifyToken, (req, res) => {
+    jwt.verify(req.token, options.skey, (err, data) => {
+        if(err){
+            res.status(403).json({
+                message: "Invalid Token !!",        
+              });
+        } else {
+            var uid = req.params.uid;
+            itembasedetails.findOne( {uniqueId: uid},  (err, result) => {
+                if (err) {
+                    res.send(err);
+                } else {
+                    res.status(200).json(result);
+                }
+            });
+        };
+    });
+});
+
+//Application Routes - End
+
+
+// Test Routes - START
+app.post("/testtoken", verifyToken, (req, res, next) => {
+    jwt.verify(req.token, options.skey, (err, data) => {
+        if(err){
+            res.status(403).json({
+                message: "Invalid Token !!",        
+              });
+        } else {
+
             res.status(200).json({
-                message: "Token validated!!",        
+                message: "Token validated 11 !!", 
+                addFn2: addFn(1,20)       
               });
         }            
     })
 });
 
+app.get("/test", (req, res) => {
+    res.send("test");
+});
+// Test Routes - END
+
+//Middle ware
 function verifyToken(req, res, next) {
 
     const bearerHeader = req.headers["authorization"];
@@ -125,35 +185,8 @@ function verifyToken(req, res, next) {
   
     }
   
-  }
-
-//This should work only after JWT is valid
-app.get("dashboard", (req, res) => {
-    // chck for JWT validation
-});
+}
 
 app.listen(9400, () => {
     console.log("hi from Node!!")
-})
-
-//module.exports = index;
-
-/*
-var fs = require('fs');
-var http = require('http');
-var https = require('https');
-var privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
-var certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
-
-var credentials = {key: privateKey, cert: certificate};
-var express = require('express');
-var app = express();
-
-// your express configuration here
-
-var httpServer = http.createServer(app);
-var httpsServer = https.createServer(credentials, app);
-
-httpServer.listen(8080);
-httpsServer.listen(8443);
-*/
+});
